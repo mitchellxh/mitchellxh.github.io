7f50cde1135ebd56d7e0ccee47a8186d15c140a1
mitchellxh <mitchellxh@gmail.com>
2024-11-25 11:02:00 -0500

    cleaned up publications and some direct links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 // App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar'; // Import the Navbar component
 import MainContent from './MainContent'; // Import the MainContent component
+import Publications from './Publications'; // New component for publications
 
 import './App.css'; // Global styles (if any)
 
 const App = () => {
   return (
-    <div>
-      {/* Navbar at the top of the page */}
-      <Navbar />
-
-      {/* Main content with text and image */}
-      <MainContent />
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/publications" element={<Publications />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
